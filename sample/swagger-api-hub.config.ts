6589cb4c65585a31fca88a41030c124b6f58fffe
李凤宝(Leo) <shijistar@gmail.com>
2025-12-18 19:21:20 +0800

    chore: update swagger-typescript-api to v13.2.16
    feat: refactor swagger-api-hub.config.ts to use common configuration for services
    
    fix: include sample directory in eslint tsconfig
    
    chore: add biome configuration file

--- sample/swagger-api-hub.config.ts
+++ sample/swagger-api-hub.config.ts
@@ -1,16 +1,17 @@
-import type { ServiceConfig } from '../cli/types';
+import type { ServiceConfig } from '../cli';
 
-const services: ServiceConfig[] = [
-  {
-    id: 'iam',
-    name: 'User Management Service',
-    url: 'https://api.example.com/iam/swagger/v3',
-  },
-  {
-    id: 'asset',
-    name: 'Asset Management Service',
-    url: 'https://api.example.com/asset/swagger/v3',
-    apiBase: '/asset',
-  },
-];
-export default services;
+const commonConfig: Partial<ServiceConfig> = {
+  httpClientFile: '../request/swagger-request',
+};
+const service: ServiceConfig = {
+  ...commonConfig,
+  id: 'iam',
+  name: 'User Management Service',
+  output: './api/iam',
+  url: 'https://api.example.com/iam/swagger/v3',
+  intTotalElements: true,
+  extractRequestBody: true,
+  extractRequestParams: false,
+  extractResponseBody: false,
+};
+export default service;
